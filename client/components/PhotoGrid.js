4cb540c0f9c7f8b106a8cf5dd6a4c6c1de0e64d8
3dw1nM0535 <workockmoses@gmail.com>
2017-12-06 17:18:18 +0300

    layout for our photos app

--- client/components/PhotoGrid.js
+++ client/components/PhotoGrid.js
@@ -1,12 +1,13 @@
 //PhotoGrid component
 import React from 'react';
 import createClass from 'create-react-class';
+import Photo from './Photo';
 
 const PhotoGrid = createClass({
   render () {
     return (
       <div className="photo-grid">
-      I am the photo grid components
+      {this.props.posts.map((post, i) => <Photo {...this.props} key={i} i={i} post={post} />)}
       </div>
     );
   }
