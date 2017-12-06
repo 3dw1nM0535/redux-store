e966abd2f2c482e663c950895b32bab40de55db8
3dw1nM0535 <workockmoses@gmail.com>
2017-12-06 17:53:40 +0300

    implemented switch action case

--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -1,12 +1,21 @@
 //reducer takes in two parameters
-//Also reducers handle all the diting of
+//Also reducers handle all the editing of
 //our App state
 
 //1. the action(info about what happened)
 //2. copy of current state
 
 export function posts (state = [], action) {
-  console.log('This post will change!');
-  console.log(state, action);
-  return state;
+  switch (action.type) {
+    case 'INCREMENT_LIKES':
+      console.log('Incrementing likes!');
+      const i = action.index;
+      return [
+        ...state.slice(0, i), //before the one we are updating
+        {...state[i], likes: state[i].likes + 1},
+        ...state.slice(i + 1), //after the one we are updating
+      ]
+    default:
+      return state;
+  }
 }
