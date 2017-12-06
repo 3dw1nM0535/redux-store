357d940ef289887277929885dc8fc82f1ea44d47
3dw1nM0535 <workockmoses@gmail.com>
2017-12-06 15:03:47 +0300

    redefined non-default exports

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,10 +1,10 @@
 //This is the root reducer which returns one big
 //data object for our app state
 import { combineReducers } from 'redux';
-import {routerReducer } from 'react-router-redux';
+import { routerReducer } from 'react-router-redux';
 
-import posts from './posts';
-import comments from './comments';
+import { posts } from './posts';
+import { comments } from './comments';
 
 export const rootReducer = combineReducers({ posts, comments, router: routerReducer });
 
