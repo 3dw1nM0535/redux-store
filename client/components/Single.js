8a97605be0cc12879ec86e0cf680d1f2cce07a7e
3dw1nM0535 <workockmoses@gmail.com>
2017-12-06 18:38:31 +0300

    single item component render

--- client/components/Single.js
+++ client/components/Single.js
@@ -1,16 +1,23 @@
 //Single photo component
 import React from 'react';
 import createClass from 'create-react-class';
+import Photo from './Photo';
+import Comment from './Comments';
 
 const Single = createClass({
   render () {
+    const postId = this.props.params.postId;
+    const i = this.props.posts.findIndex((post) =>
+      post.code === postId);
+    const post = this.props.posts[i];
+    const postComments = this.props.comments[postId] || [];
     return (
       <div className="single-photo">
-      I'm the single photo component
+        <Photo i={i} post={post} {...this.props} />
+        <Comment postComments={postComments} />
       </div>
     );
   }
 });
 
 export default Single;
-
