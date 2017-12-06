45720a1fa32fde93877bb4ecfeff6fc1f71a6955
3dw1nM0535 <workockmoses@gmail.com>
2017-12-06 14:46:26 +0300

    created root reducer

---
+++ client/reducers/comments.js
@@ -0,0 +1,6 @@
+//comments reducers
+function comments (state = [], action) {
+  console.log(state, action);
+  return state;
+}
+
---
+++ client/reducers/index.js
@@ -0,0 +1,12 @@
+//This is the root reducer which returns one big
+//data object for our app state
+import { combineReducers } from 'redux';
+import {routerReducer } from 'react-router-redux';
+
+import posts from './posts';
+import comments from './comments';
+
+export const rootReducer = combineReducers({ posts, comments, router: routerReducer });
+
+
+
