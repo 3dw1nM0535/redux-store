3a483d8d6b254db0bde267da191a1ab3e6cdc6b8
3dw1nM0535 <workockmoses@gmail.com>
2017-12-06 19:28:25 +0300

    defined remove comment Midleware

--- client/reducers/comments.js
+++ client/reducers/comments.js
@@ -1,6 +1,39 @@
 //comments reducers
-export function comments (state = [], action) {
-  console.log(state, action);
+//add comment on the basis of function recomposition
+function postComment(state=[], action) {
+  switch (action.type) {
+    case 'ADD_COMMENT':
+      //return the new state with the new comment
+      return [...state, {
+        user: action.author,
+        text: action.comment
+      }];
+    case 'REMOVE_COMMENT':
+      //return new state without the deleted comment
+      return [
+        //from start to the one we want to delete
+        ...state.slice(0, action.i),
+        //after the deleted one to the end
+        ...state.slice(action.i + 1)
+      ]
+
+    default:
+      return state;
+
+  }
+
   return state;
 }
 
+export function comments (state = [], action) {
+  if (typeof action.postId !== 'undefined') {
+    return {
+      //take the current state
+      ...state,
+      //overwrite this post with a new one
+      [action.postId]: postComment(state[action.postId], action)
+    }
+  }
+
+  return state;
+}
