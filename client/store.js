17d6852d7104d98bfdc1534542ddfd6482fc175f
3dw1nM0535 <workockmoses@gmail.com>
2017-12-06 14:08:57 +0300

    defined app data store

---
+++ client/store.js
@@ -0,0 +1,21 @@
+//Redux starter file
+import { createStore, compose } from 'redux';
+import { syncHistoryWithStore } from 'react-router-redux';
+import { browserHistory } from 'react-router';
+
+//import root reducer
+import rootReducer from './reducers/index';
+
+import comments from './data/comments';
+import posts from './data/posts';
+
+//create object for default data
+const defaultState = {
+  posts, comments
+};
+
+export const store = createStore(rootReducer, defaultState);
+
+export const history = syncHistoryWithStore(browserHistory, store);
+
+
