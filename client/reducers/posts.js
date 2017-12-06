4503b4cef85ef0d9571c7b2034135b37a99dc37e
3dw1nM0535 <workockmoses@gmail.com>
2017-12-06 16:24:16 +0300

    posts reducer function

--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -1,10 +1,12 @@
 //reducer takes in two parameters
+//Also reducers handle all the diting of
+//our App state
 
 //1. the action(info about what happened)
 //2. copy of current state
 
 export function posts (state = [], action) {
+  console.log('This post will change!');
   console.log(state, action);
   return state;
 }
-
